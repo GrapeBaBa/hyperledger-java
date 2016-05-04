2d8d26598841fee0c0418a09043832515025a859
grapebaba <281165273@qq.com>
2016-05-04 11:23:34 +0800

    refine

--- hyperledger-java-examples/src/main/java/io/grapebaba/hyperledger/fabric/examples/FabricExample.java
+++ hyperledger-java-examples/src/main/java/io/grapebaba/hyperledger/fabric/examples/FabricExample.java
@@ -1,18 +1,19 @@
 /*
-Copyright 2016 281165273@qq.com
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not
-use this file except in compliance with the License. You may obtain a copy of
-the License at
-
-    [http://www.apache.org/licenses/LICENSE-2.0]
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-License for the specific language governing permissions and limitations under
-the License.
-*/
+ * Copyright 2016 281165273@qq.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 package io.grapebaba.hyperledger.fabric.examples;
 
@@ -87,7 +88,7 @@ public class FabricExample {
                     public Observable<Transaction> call(ChaincodeOpResult chaincodeOpResult) {
                         System.out.printf("Invoke chaincode result:%s\n", chaincodeOpResult);
                         try {
-                            TimeUnit.SECONDS.sleep(1L);
+                            TimeUnit.SECONDS.sleep(3L);
                         } catch (InterruptedException e) {
 
                         }
