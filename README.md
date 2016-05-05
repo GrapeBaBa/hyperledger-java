9812af08f0f7e6a3b86987e9e04ed46dd476c975
grapebaba <281165273@qq.com>
2016-05-05 12:05:52 +0800

    remove lombok

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Hyperledger Java SDK
+A java client SDK for Hyperledger
+===============================
+
+----------
+
+Easy to use
+----------------------------------------------------------
+
+      FABRIC.getNetworkPeers().subscribe(new Action1<PeersMessage>() {
+            @Override
+            public void call(PeersMessage peersMessage) {
+                for (PeerEndpoint peerEndpoint : peersMessage.getPeers()) {
+                    System.out.printf("Peer message:%s\n", peerEndpoint);
+                }
+
+            }
+        });
--- hyperledger-java-examples/src/main/java/io/grapebaba/hyperledger/fabric/examples/FabricExample.java
+++ hyperledger-java-examples/src/main/java/io/grapebaba/hyperledger/fabric/examples/FabricExample.java
@@ -193,16 +193,15 @@ public class FabricExample {
                     }
                 });
 
-        FABRIC.getNetworkPeers()
-                .subscribe(new Action1<PeersMessage>() {
-                    @Override
-                    public void call(PeersMessage peersMessage) {
-                        for (PeerEndpoint peerEndpoint : peersMessage.getPeers()) {
-                            System.out.printf("Peer message:%s\n", peerEndpoint);
-                        }
+        FABRIC.getNetworkPeers().subscribe(new Action1<PeersMessage>() {
+            @Override
+            public void call(PeersMessage peersMessage) {
+                for (PeerEndpoint peerEndpoint : peersMessage.getPeers()) {
+                    System.out.printf("Peer message:%s\n", peerEndpoint);
+                }
 
-                    }
-                });
+            }
+        });
 
         FABRIC.deleteRegistrar("lukas").subscribe(new Action1<OK>() {
             @Override
