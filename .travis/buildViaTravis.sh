a2fd951cc183c4185c16fd3e624b0f5461159814
grapebaba <281165273@qq.com>
2016-05-09 09:19:34 +0800

    add bintray

--- .travis/buildViaTravis.sh
+++ .travis/buildViaTravis.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   #./gradlew -Prelease.travisci=true -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PsonatypeUsername="${SONATYPE_USER}" -PsonatypePassword="${SONATYPE_PASSWORD}" build snapshot --stacktrace
-  ./gradlew -Prelease.travisci=true -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}"  build snapshot --stacktrace
+  ./gradlew -Prelease.travisci=true -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" snapshot --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_PASSWORD}" -PsonatypeUsername="${SONATYPE_USER}" -PsonatypePassword="${SONATYPE_PASSWORD}" final --stacktrace
